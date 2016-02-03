71852df2839bbc3c3526202ec4a008a41e507bf2
kKhilko <kan.hilko@gmail.com>
2016-02-02 19:24:00 -0800

    Added contest.py

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
 
-from fixture.application import Application
 from model.contact import Contact
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_new_contact(app):
     app.session.login("admin", "secret")
