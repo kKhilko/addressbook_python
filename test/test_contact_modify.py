72f279e3e9c61ccc6ec4e234bb8431328f524781
kKhilko <kan.hilko@gmail.com>
2016-02-10 18:30:42 -0800

    Add preconditions for modify_groups and modify_contacts

--- test/test_contact_modify.py
+++ test/test_contact_modify.py
@@ -3,10 +3,16 @@ from model.contact import Contact
 
 
 def test_modify_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create_new(Contact(firstname="test_new_contact"))
     app.contact.modify_first_contact(Contact(firstname="new_firstname"))
 
 def test_modify_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create_new(Contact(firstname="test_new_contact"))
     app.contact.modify_first_contact(Contact(lastname="new_lastname"))
 
 def test_modify_company(app):
+    if app.contact.count() == 0:
+        app.contact.create_new(Contact(firstname="test_new_contact"))
     app.contact.modify_first_contact(Contact(company="new_company"))
