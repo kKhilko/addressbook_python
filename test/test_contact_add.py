88de3a9118d6dc225de3915f64da9996de31d1b7
kKhilko <kan.hilko@gmail.com>
2016-03-03 17:24:11 -0800

    Updated test for add contact

--- test/test_contact_add.py
+++ test/test_contact_add.py
@@ -1,36 +1,10 @@
 # -*- coding: utf-8 -*-
-import random
-import string
 
-import pytest
 from model.contact import Contact
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join(random.choice(symbols)for i in range(random.randrange(maxlen)))
 
-## execute simple 2 test scenario
-# testdata = [
-#     Contact(firstname="test_new_contact", lastname="test", company="abc"),
-#     Contact(firstname="", lastname="", company="")
-# ]
-
-# # execute 1 simple empty test and 5 tests with random data
-# testdata = [Contact(firstname="", lastname="", company="")] + [
-#      Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 20), company=random_string("company", 20))
-#      for i in range(5)
-#  ]
-
-# execute data combination for parameters (total)
-testdata = [
-    Contact(firstname=firstname, lastname=lastname, company=company)
-    for firstname in ['', random_string("firstname", 10)]
-    for lastname in ['', random_string("lastname", 10)]
-    for company in ['', random_string("company", 10)]
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_contact_add_new(app, contact):
+def test_contact_add_new(app, json_contact):
+    contact = json_contact
     old_contacts = app.contact.get_list()
     app.contact.create_new(contact)
     assert len(old_contacts)+1 == app.contact.count()
