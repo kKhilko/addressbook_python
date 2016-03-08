8063726669b79fc074492908f1638216719caa3c
kKhilko <kan.hilko@gmail.com>
2016-03-08 11:25:25 -0800

    Added driver for work with mysql

---
+++ check_db_connection.py
@@ -0,0 +1,11 @@
+from mysql import connector
+
+connection = connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
